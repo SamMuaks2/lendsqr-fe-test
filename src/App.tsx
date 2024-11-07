99d25ac020b7e2b2c56d9a72d9cfac5fc0dd1ed7
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-11-07 10:24:33 +0000

    Add router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import UserDetailsPage from "./pages/UserDetailsPage";
-// import UserPage from "./pages/UserPage";
-// import Login from "./pages/Login";
-// import Dashboard from "./pages/Dashboard";
+import UserPage from "./pages/UserPage";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <>
-      {/* <Login /> */}
-      {/* <Dashboard /> */}
-      {/* <UserPage /> */}
-      <UserDetailsPage />
-    </>
+    <Router>
+      <Routes>
+       <Route path="/" element={<Login />} />
+       <Route path="/dashboard" element={<Dashboard />} />
+       <Route path="/user" element={<UserPage />} />
+       <Route path="/user/:id" element={<UserDetailsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
